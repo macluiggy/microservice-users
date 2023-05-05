04cb8f7b3cc7bfe188047daa5b69e4f674215bcb
macluiggy <luiggy.macias@imagineapps.co>
2023-05-04 20:35:09 -0500

    T => t

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
 import { RabbitMQ } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
-    Transport: Transport.RMQ,
+    transport: Transport.RMQ,
     options: {
       urls: [process.env.AMQP_URL],
       queue: RabbitMQ.UserQueue,
     },
   });
   await app.listen();
+  console.log('Microservice Users is listening');
 }
-bootstrap();
+bootstrap(); 
