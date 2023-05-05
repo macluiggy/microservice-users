c94fdd62c19d5726448b865fafe645b9581407fb
macluiggy <luiggy.macias@imagineapps.co>
2023-05-04 20:04:58 -0500

    user controller as a microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.production'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,32 +10,34 @@ import {
 } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { UserMSG } from 'src/common/constants';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Post()
-  create(@Body() userDto: UserDto) {
+  @MessagePattern(UserMSG.CREATE)
+  create(@Payload() userDto: UserDto) {
     return this.userService.create(userDto);
   }
 
-  @Get()
+  @MessagePattern(UserMSG.FIND_ALL)
   findAll() {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @MessagePattern(UserMSG.FIND_ONE)
+  findOne(@Payload() id: string) {
     return this.userService.findOne(id);
   }
 
-  @Put(':id')
-  update(@Param('id') id: string, @Body() userDto: UserDto) {
+  @MessagePattern(UserMSG.UPDATE)
+  update(@Payload() { id, userDto }: { id: string; userDto: UserDto }) {
     return this.userService.update(id, userDto);
   }
 
-  @Delete(':id')
-  delete(@Param('id') id: string) {
+  @MessagePattern(UserMSG.DELETE)
+  delete(@Payload() id: string) {
     return this.userService.delete(id);
   }
 }
